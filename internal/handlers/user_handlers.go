cb71e6804948e02753455ae405fb63385c6e1b04
Yoann <yoann.letacq0@gmail.com>
2025-02-06 19:43:18 +0100

    CREATE LoginHandler with GenerateJWt function; not tested yet.

--- internal/handlers/user_handlers.go
+++ internal/handlers/user_handlers.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"YoannLetacq/todo-api.git/config"
 	"YoannLetacq/todo-api.git/internal/models"
+	"YoannLetacq/todo-api.git/internal/utils"
 	"log"
 	"net/http"
+	"strconv"
 
 	"golang.org/x/crypto/bcrypt"
 
@@ -50,5 +52,49 @@ func RegisterUser(c *gin.Context) {
 	c.JSON(
 		http.StatusCreated,
 		gin.H{"message": "Utilisateur enregistré avec succès !"},
+	)
+}
+
+// gèrela connexion de l'utilisateur
+func LoginHandler(c *gin.Context) {
+	var req models.User
+	var user models.User
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": "Requête invalide"},
+		)
+		return
+	}
+
+	if err := config.DB.Where("email = ?", req.Email).First(&user).Error; err != nil {
+		c.JSON(
+			http.StatusUnauthorized,
+			gin.H{"error": "Utilisateur non trouve."},
+		)
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(req.Password), []byte(user.Password)); err != nil {
+		c.JSON(
+			http.StatusUnauthorized,
+			gin.H{"error": " Mot de passe invalide."},
+		)
+		return
+	}
+
+	token, err := utils.GenerateJWT(strconv.Itoa(int(user.ID)), user.Email)
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "Echec de la création du token jwt."},
+		)
+		return
+	}
+
+	c.JSON(
+		http.StatusOK,
+		gin.H{"token": token},
 	)
 }
